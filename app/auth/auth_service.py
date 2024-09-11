12474587ca597247bd60f3a54408fc2b7320251a
saadchohan4118 <saadchohan4118@gmail.com>
2024-09-11 22:07:45 +0500

    up

--- app/auth/auth_service.py
+++ app/auth/auth_service.py
@@ -1,10 +1,13 @@
 from datetime import datetime
+import random
 from fastapi import HTTPException,status,BackgroundTasks
 from app import smtp, utils
 from app.auth.dtos.login_user import LoginUserDTO
 from app.auth.dtos.register_user import RegisterUserDTO
 from app.database import Users
 from app.users.models.user import User
+import uuid
+
 
 class AuthService:
 
@@ -19,8 +22,11 @@ class AuthService:
         userJSON['password'] = hashedPassword
         userJSON['created_on'] = datetime.now()
         userJSON['modified_on'] = datetime.now()
+        userJSON['isEmailVerified'] = False 
+        userJSON['emailVerificationLinkToken'] = str(uuid.uuid4()) 
+        userJSON['emailVerificationCode'] = str(random.randint(1000, 9999)) 
         user = await Users.insert_one(userJSON)
-        background_tasks.add_task(smtp.send_email, register_user.email, 'Welcome', 'Welcome To Voxera')
+        background_tasks.add_task(smtp.send_email, register_user.email, 'Welcome', 'Welcome To Voxera '+userJSON['emailVerificationLinkToken'] + userJSON['emailVerificationCode'])
         return {'userId':str(user.inserted_id)}
 
     async def login_user(self,login_user:LoginUserDTO):
@@ -29,6 +35,8 @@ class AuthService:
             raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="User Not Found.")
         dbUser['id'] = str(dbUser['_id'])
         user = User(**dbUser)
+        if user.isEmailVerified is False:
+            raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,detail="Please Verify Your Email.")
         flag = utils.verify_password(login_user.password,user.password)
         if flag is False:
             raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,detail="Invalid Credentials")
--- app/users/models/user.py
+++ app/users/models/user.py
@@ -3,5 +3,6 @@ from app.auth.dtos.register_user import RegisterUserDTO
 class User(RegisterUserDTO):
     id:str
     isEmailVerified: bool = False
-    
+    emailVerificationLinkToken: str|None=None
+    emailVerificationCode:str|None = None
     
