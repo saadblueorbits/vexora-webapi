63c36c1fea89e43fd324e2f93b721625fc654cfa
saadchohan4118 <saadchohan4118@gmail.com>
2024-09-21 03:23:39 +0500

    healthcheck added

--- app/main.py
+++ app/main.py
@@ -15,7 +15,7 @@ app.include_router(router=auth_controller.router, tags=['Auth'], prefix='/api/au
 app.include_router(router=users_controller.router, tags=['Users'], prefix='/api/users')
 app.include_router(router=speaker_controller.router, tags=['Speakers'], prefix='/api/speakers')
 
-@app.get("/api/")
+@app.get("/")
 def health_check():
     return "running"
 
