d175c94b7b6108e9ca7ac4b49fd0a86ef3554e9e
saadchohan4118 <saadchohan4118@gmail.com>
2024-09-21 03:26:10 +0500

    healthcheck added

--- app/database.py
+++ app/database.py
@@ -1,6 +1,10 @@
 from motor.motor_asyncio import AsyncIOMotorClient
 from app.config import settings
 
+
+print("DB URL : "+settings.DATABASE_URL)
+print("DATABASE : "+settings.MONGO_INITDB_DATABASE)
+
 client = AsyncIOMotorClient(settings.DATABASE_URL)
 
 db= client.get_database(settings.MONGO_INITDB_DATABASE)
